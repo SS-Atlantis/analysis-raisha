d0376e8bfcf24e1968f1973bd7a92cc339ef028b
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-04-13 11:52:20 -0700

    parcels 10K 5b_TurnPoint_Dilbit 2020-01-16

--- notebooks/contaminant-dispersal/raisha_parcels.py
+++ notebooks/contaminant-dispersal/raisha_parcels.py
@@ -29,8 +29,8 @@ from util.seed_particles import get_particles, get_release_times
 
 # %%
 # Spill release times (years 2019 and 2020 only)
-release_start_time = '2020-01-11' 
-release_end_time = '2020-01-12'
+release_start_time = '2020-01-16' 
+release_end_time = '2020-01-17'
 release_YYYY = '2020-01'
 
 # %% [markdown]
