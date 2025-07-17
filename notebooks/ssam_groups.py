a6316181324c4d5577b59285aa1a569039f0ecc6
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2025-07-16 20:26:23 -0700

    add conditions, update bio group model sensitivity

--- notebooks/ssam_groups.py
+++ notebooks/ssam_groups.py
@@ -35,8 +35,17 @@ named_fish = {
 
 salmon = {
     'Chinook':'Chinook_salmon', 'Chum':'Chum_salmon', 'Coho':'Coho_salmon','Pink':'Pink_salmon', 'Sockeye':'Sockeye_salmon', 
-    'Hatchery Chinook':'Hatchery_Chinook_salmon','Hatchery Coho':'Hatchery_Coho_salmon','Hatchery Sockeye':'Hatchery_Sockeye_salmon'
-    #,'Other salmonids':'Other_salmonids'
+    'Hatchery Chinook':'Hatchery_Chinook_salmon','Hatchery Coho':'Hatchery_Coho_salmon','Hatchery Sockeye':'Hatchery_Sockeye_salmon',
+    'Other salmonids':'Other_salmonids'
+}
+
+wild_salmon = {
+    'Chinook':'Chinook_salmon', 'Chum':'Chum_salmon', 'Coho':'Coho_salmon','Pink':'Pink_salmon', 'Sockeye':'Sockeye_salmon', 
+    'Other salmonids':'Other_salmonids'
+}
+
+hatchery_salmon = {
+    'Hatchery Chinook':'Hatchery_Chinook_salmon','Hatchery Coho':'Hatchery_Coho_salmon','Hatchery Sockeye':'Hatchery_Sockeye_salmon',
 }
 
 benthic_invertebrates = { #benthic, 1 depth
@@ -49,7 +58,7 @@ macroalgae = { #benthic, 1 depth
 }
 
 sediment_feeders = { #pelagic, i.e. have > 1 depth level but only exist in the sediment
-    'benthic carnivores':'Benthic_Carniv', 'deposit feeders':'Deposit_Feeder', 'meiobenthos':'Meiobenth', 'sedimentary bacteria': 'Sed_Bact',
+    'benthic carnivores':'Benthic_Carniv', 'deposit feeders':'Deposit_Feeder', 'sedimentary bacteria': 'Sed_Bact',
 }
     
 shellfish = { #benthic, 1 depth
@@ -62,7 +71,7 @@ plankton = {
 }
 
 pelagic_invertebrates = {
-    'squid':'Squid', 'prawn': 'Prawn', 
+    'squid':'Squid', #'prawn': 'Prawn', 
 }
 
 bacteria = {
@@ -75,7 +84,7 @@ phytoplankton = {
 }
 
 zooplankton = {
-    'carnivorous zooplankton':'Carniv_Zoo', 'gelatinous zooplantkon':'Gelat_Zoo', 'microzooplankton':'MicroZoo',
+    'microzooplankton':'MicroZoo', #'carnivorous zooplankton':'Carniv_Zoo', 'gelatinous zooplantkon':'Gelat_Zoo', 
     'mesozooplankton':'Zoo',
 }
 
@@ -111,15 +120,36 @@ all_pelagic = {
     'squid':'Squid', 'prawn': 'Prawn', 
 }
 
+all_pelagic_verts = {
+    'dogfish':'Dogfish','ratfish':'Ratfish', 'sixgill':'Sixgill', 'skates':'Skates',
+    'seagulls':'Seabird_gulls', 'other seabirds':'Seabirds',
+    'porpoises':'Harbor_porpoise', 'seals':'Harbor_seals', 'humpbacks':'Humpback_whales','sealions':'Sealions',
+    'resident orcas':'Orcas_resident', 'transient orcas':'Orcas_transient',
+    'benthopelagic':'Benthopelagic', 'small pelagic':'Small_pelagic_fish', 'small demersal':'Small_Demersal_fish', 
+    'large demersal':'Large_Demersal_fish', 'small flatfish':'Small_Flatfish', 'large flatfish':'Large_Flatfish',
+    'hake':'Hake','lingcod':'Lingcod', 'pollock':'Pollock', 'sandlance':'Sandlance', 
+    'rockfish':'Rockfish', 'pacific herring':'Pacific_herring',
+    'Chinook':'Chinook_salmon', 'Chum':'Chum_salmon', 'Coho':'Coho_salmon','Pink':'Pink_salmon', 'Sockeye':'Sockeye_salmon', 
+    'Hatchery Chinook':'Hatchery_Chinook_salmon','Hatchery Coho':'Hatchery_Coho_salmon','Hatchery Sockeye':'Hatchery_Sockeye_salmon', 
+    'Other salmonids':'Other_salmonids', 
+}
+
+large_verts = {
+    'porpoises':'Harbor_porpoise', 'seals':'Harbor_seals', 'humpbacks':'Humpback_whales','sealions':'Sealions',
+    'resident orcas':'Orcas_resident', 'transient orcas':'Orcas_transient',
+    'dogfish':'Dogfish','ratfish':'Ratfish', 'sixgill':'Sixgill', 'skates':'Skates',
+    'seagulls':'Seabird_gulls', 'other seabirds':'Seabirds',
+}
+
 nutrients = {
     'ammonia':'NH3', 'nitrate':'NO3', 'dissolved organic nitrogen':'DON', 'micronutrients': 'MicroNut', 
     'dissolved silica':'Si', 'detrital silica':'Det_Si',
 }
 
 planktonic = {
-    'carnivorous zooplankton':'Carniv_Zoo', 'gelatinous zooplantkon':'Gelat_Zoo', 'microzooplankton':'MicroZoo',
+    'microzooplankton':'MicroZoo', #'carnivorous zooplankton':'Carniv_Zoo', 'gelatinous zooplantkon':'Gelat_Zoo', 
     'mesozooplankton':'Zoo', 'diatoms':'Diatom', 'picophytoplankton':'PicoPhytopl', 'pelagic bacteria':'Pelag_Bact',
-    'squid':'Squid', 'prawn': 'Prawn', 
+    'squid':'Squid', #'prawn': 'Prawn', 
 }
 
 detritus = {
@@ -130,29 +160,42 @@ salish_sea = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 18, 19, 20, 21,
     40, 54, 55, 57, 58, 60, 62, 63, 64, 66, 68, 72, 73, 74, 75, 79, 80, 81, 82, 83, 84, 85, 86, 88, 90, 91,92, 94, 96, 98, 101, 
     102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124]
 juan_de_fuca = [1,2,3,4,5,6,7,8,9,10,11]
-haro_boundary = {60,33,34,35,36,}
+haro_boundary = [60,33,34,35,36,]
 strait_of_georgia = [54,64,68,79,80,81,82,83,84,85,88,92,98,101,102]
 sog_south = [54,64,68,79]
 sog_center = [80,81,82,83,84,85]
 sog_north = [88,92.98,101,102]
 puget_sound_main = [20,21,23,26]
 fraser_river = [75,80,81]
+active_pass = [62,63]
 
 
-sensitivity = {'carnivorous zooplankton':8.6, 'gelatinous zooplantkon':2.1, 'microzooplankton':5.8,
-    'mesozooplankton':1.7, 'diatoms':0.3, 'picophytoplankton':1.0, 'pelagic bacteria':0.8,
-    'squid':1.7, 'prawn': 3.3, 'benthic grazers':0.0, 'other filter feeders': 0.0, 'macrobenthos':0.1,  
-    'sponges':1.3, 'bivalves':0.1, 'crabs':0.6, 'dungeness crabs':0.1,
-    'macroalgae':0.2, 'seagrass':4.2, 'benthic carnivores':0.0, 'deposit feeders':0.1, 'meiobenthos':0.8, 'sedimentary bacteria': 0.6,
-    'dogfish':0.2,'ratfish':0.1, 'sixgill':0.0, 'skates':0.4,
-    'seagulls':0.3, 'other seabirds':0.2,
-    'porpoises':1.2, 'seals':0.1, 'humpbacks':0.3,'sealions':0.0,
+sensitivity = {'carnivorous zooplankton':9.0, 'gelatinous zooplantkon':0.7, 'microzooplankton':5.5,
+    'mesozooplankton':0.2, 'diatoms':0.1, 'picophytoplankton':0.3, 'pelagic bacteria':0.1,
+    'squid':0.1, 'prawn': 0.2, 'benthic grazers':9.6, 'other filter feeders': 0.0, 'macrobenthos':1.8,  
+    'sponges':0.1, 'bivalves':0.1, 'crabs':0.6, 'dungeness crabs':0.2,
+    'macroalgae':1.4, 'seagrass':6.1, 'benthic carnivores':0.0, 'deposit feeders':0.0, 'meiobenthos':0.1, 'sedimentary bacteria': 0.1,
+    'dogfish':0.1,'ratfish':0.0, 'sixgill':0.0, 'skates':0.0,
+    'seagulls':1.3, 'other seabirds':0.2,
+    'porpoises':0.4, 'seals':0.1, 'humpbacks':0.1,'sealions':0.3,
     'resident orcas':0.1, 'transient orcas':0.0,
-    'benthopelagic':0.2, 'small pelagic':2.9, 'small demersal':0.3, 
-    'large demersal':0.5, 'small flatfish':0.1, 'large flatfish':0.2,
-    'hake':0.4,'lingcod':0.4, 'pollock':0.1, 'sandlance':16, 
-    'rockfish':0.5, 'pacific herring':0.7,
-    'Chinook':0.3, 'Chum':0.6, 'Coho':0.8,'Pink':0.3, 'Sockeye':0.4, 
-    'Hatchery Chinook':0.4,'Hatchery Coho':0.5,'Hatchery Sockeye':1.6, 
-    'Other salmonids':0.0,
+    'benthopelagic':0.2, 'small pelagic':1.1, 'small demersal':0.1, 
+    'large demersal':0.3, 'small flatfish':0.6, 'large flatfish':0.1,
+    'hake':0.4,'lingcod':0.1, 'pollock':0.1, 'sandlance':0.6, 
+    'rockfish':1.2, 'pacific herring':0.2,
+    'Chinook':0.4, 'Chum':0.4, 'Coho':0.1,'Pink':0.0, 'Sockeye':0.7, 
+    'Hatchery Chinook':0.1,'Hatchery Coho':0.1,'Hatchery Sockeye':0.2, 
+    'Other salmonids':0.1,
+}
+
+conditions = {
+    '2019-01-14':'low Fraser flow, winter neap tide', '2019-01-20':'low Fraser flow, winter spring tide','2020-01-16':'high Fraser flow, winter neap tide', '2020-01-11':'high Fraser flow, winter spring tide',
+    '2019-01-26':'low Fraser flow, winter neap tide', '2019-01-21':'low Fraser flow, winter spring tide','2020-01-03':'high Fraser flow, winter neap tide',
+    '2019-07-10':'low Fraser flow, summer neap tide', '2019-07-16':'low Fraser flow, summer spring tide','2020-07-12':'high Fraser flow, summer neap tide', '2020-07-20':'high Fraser flow, summer spring tide',
+    '2019-07-25':'low Fraser flow, summer neap tide', '2020-07-13':'high Fraser flow, summer neap tide', '2020-07-21':'high Fraser flow, summer spring tide',
+    
+}
+
+locations = {
+    '5b': 'Turn Point', '4a': 'Active Pass', '6a': 'English Bay', '7a': 'Juan de Fuca'
 }
