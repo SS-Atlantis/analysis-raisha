7a8de7bf55bf233d994c4963b2b896a8eb9dccdd
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2025-10-03 14:39:50 -0700

    consolidate plankton groups

--- notebooks/ssam_groups.py
+++ notebooks/ssam_groups.py
@@ -84,7 +84,7 @@ phytoplankton = {
 }
 
 zooplankton = {
-    'microzooplankton':'MicroZoo', #'carnivorous zooplankton':'Carniv_Zoo', 'gelatinous zooplantkon':'Gelat_Zoo', 
+    'microzooplankton':'MicroZoo', 'carnivorous zooplankton':'Carniv_Zoo', 'gelatinous zooplantkon':'Gelat_Zoo', 
     'mesozooplankton':'Zoo',
 }
 
@@ -103,21 +103,28 @@ primaryproducer = {
 }
 
 all_pelagic = {
-    'dogfish':'Dogfish','ratfish':'Ratfish', 'sixgill':'Sixgill', 'skates':'Skates',
-    'seagulls':'Seabird_gulls', 'other seabirds':'Seabirds',
-    'porpoises':'Harbor_porpoise', 'seals':'Harbor_seals', 'humpbacks':'Humpback_whales','sealions':'Sealions',
-    'resident orcas':'Orcas_resident', 'transient orcas':'Orcas_transient',
-    'benthopelagic':'Benthopelagic', 'small pelagic':'Small_pelagic_fish', 'small demersal':'Small_Demersal_fish', 
-    'large demersal':'Large_Demersal_fish', 'small flatfish':'Small_Flatfish', 'large flatfish':'Large_Flatfish',
+    'pelagic bacteria':'Pelag_Bact','picophytoplankton':'PicoPhytopl', 'diatoms':'Diatom','microzooplankton':'MicroZoo', 
+    #'carnivorous zooplankton':'Carniv_Zoo', 'gelatinous zooplantkon':'Gelat_Zoo', 
+    'mesozooplankton':'Zoo', 'squid':'Squid', 'prawn': 'Prawn', 
     'hake':'Hake','lingcod':'Lingcod', 'pollock':'Pollock', 'sandlance':'Sandlance', 
-    'rockfish':'Rockfish', 'pacific herring':'Pacific_herring',
+     'pacific herring':'Pacific_herring', 
     'Chinook':'Chinook_salmon', 'Chum':'Chum_salmon', 'Coho':'Coho_salmon','Pink':'Pink_salmon', 'Sockeye':'Sockeye_salmon', 
     'Hatchery Chinook':'Hatchery_Chinook_salmon','Hatchery Coho':'Hatchery_Coho_salmon','Hatchery Sockeye':'Hatchery_Sockeye_salmon', 
     'Other salmonids':'Other_salmonids',
+    'small pelagic':'Small_pelagic_fish', 'rockfish':'Rockfish',
+    'small demersal':'Small_Demersal_fish', 'small flatfish':'Small_Flatfish', 
+    'benthopelagic':'Benthopelagic', 'large demersal':'Large_Demersal_fish', 'large flatfish':'Large_Flatfish',
+    'dogfish':'Dogfish','ratfish':'Ratfish', 'sixgill':'Sixgill', 'skates':'Skates',
+    'porpoises':'Harbor_porpoise', 'seals':'Harbor_seals', 'sealions':'Sealions',
+    'resident orcas':'Orcas_resident', 'transient orcas':'Orcas_transient', 'humpbacks':'Humpback_whales',
+    'seagulls':'Seabird_gulls', 'other seabirds':'Seabirds',
+    
+    
+    
+    
     'benthic carnivores':'Benthic_Carniv', 'deposit feeders':'Deposit_Feeder', 'meiobenthos':'Meiobenth', 'sedimentary bacteria': 'Sed_Bact',
-    'carnivorous zooplankton':'Carniv_Zoo', 'gelatinous zooplantkon':'Gelat_Zoo', 'microzooplankton':'MicroZoo',
-    'mesozooplankton':'Zoo', 'diatoms':'Diatom', 'picophytoplankton':'PicoPhytopl', 'pelagic bacteria':'Pelag_Bact', 
-    'squid':'Squid', 'prawn': 'Prawn', 
+ 
+    
 }
 
 all_pelagic_verts = {
@@ -152,6 +159,11 @@ planktonic = {
     'squid':'Squid', #'prawn': 'Prawn', 
 }
 
+other_planktonic = {
+    'pelagic bacteria':'Pelag_Bact',
+    'squid':'Squid', #'prawn': 'Prawn', 
+}
+
 detritus = {
     'labile detritus':'Lab_Det','refractory detritus':'Ref_Det',
 }
@@ -171,31 +183,42 @@ active_pass = [62,63]
 
 
 sensitivity = {'carnivorous zooplankton':9.0, 'gelatinous zooplantkon':0.7, 'microzooplankton':5.5,
-    'mesozooplankton':0.2, 'diatoms':0.1, 'picophytoplankton':0.3, 'pelagic bacteria':0.1,
-    'squid':0.1, 'prawn': 0.2, 'benthic grazers':9.6, 'other filter feeders': 0.0, 'macrobenthos':1.8,  
+    'mesozooplankton':0.2, 'Zooplankton':0.2, 'diatoms':0.1, 'picophytoplankton':0.3, 'Phytoplankton':0.3,
+    'pelagic bacteria':0.1,
+    'squid':0.1, 'prawn': 0.2, 'benthic grazers':9.6, 'other filter feeders': 0.1, 'macrobenthos':1.8,  
     'sponges':0.1, 'bivalves':0.1, 'crabs':0.6, 'dungeness crabs':0.2,
-    'macroalgae':1.4, 'seagrass':6.1, 'benthic carnivores':0.0, 'deposit feeders':0.0, 'meiobenthos':0.1, 'sedimentary bacteria': 0.1,
-    'dogfish':0.1,'ratfish':0.0, 'sixgill':0.0, 'skates':0.0,
+    'macroalgae':1.4, 'seagrass':6.1, 'benthic carnivores':0.1, 'deposit feeders':0.1, 'meiobenthos':0.1, 
+    'sedimentary bacteria': 0.1,
+    'dogfish':0.1,'ratfish':0.1, 'sixgill':0.1, 'skates':0.1,
     'seagulls':1.3, 'other seabirds':0.2,
     'porpoises':0.4, 'seals':0.1, 'humpbacks':0.1,'sealions':0.3,
-    'resident orcas':0.1, 'transient orcas':0.0,
+    'resident orcas':0.1, 'transient orcas':0.1,
     'benthopelagic':0.2, 'small pelagic':1.1, 'small demersal':0.1, 
     'large demersal':0.3, 'small flatfish':0.6, 'large flatfish':0.1,
     'hake':0.4,'lingcod':0.1, 'pollock':0.1, 'sandlance':0.6, 
     'rockfish':1.2, 'pacific herring':0.2,
-    'Chinook':0.4, 'Chum':0.4, 'Coho':0.1,'Pink':0.0, 'Sockeye':0.7, 
+    'Chinook':0.4, 'Chum':0.4, 'Coho':0.1,'Pink':0.1, 'Sockeye':0.7, 
     'Hatchery Chinook':0.1,'Hatchery Coho':0.1,'Hatchery Sockeye':0.2, 
     'Other salmonids':0.1,
 }
 
 conditions = {
-    '2019-01-14':'low Fraser flow, winter neap tide', '2019-01-20':'low Fraser flow, winter spring tide','2020-01-16':'high Fraser flow, winter neap tide', '2020-01-11':'high Fraser flow, winter spring tide',
-    '2019-01-26':'low Fraser flow, winter neap tide', '2019-01-21':'low Fraser flow, winter spring tide','2020-01-03':'high Fraser flow, winter neap tide',
-    '2019-07-10':'low Fraser flow, summer neap tide', '2019-07-16':'low Fraser flow, summer spring tide','2020-07-12':'high Fraser flow, summer neap tide', '2020-07-20':'high Fraser flow, summer spring tide',
-    '2019-07-25':'low Fraser flow, summer neap tide', '2020-07-13':'high Fraser flow, summer neap tide', '2020-07-21':'high Fraser flow, summer spring tide',
-    
+    '2019-01-14':'low Fraser flow neap tide winter', '2019-01-20':'low Fraser flow spring tide winter',
+    '2020-01-16':'high Fraser flow neap tide winter', '2020-01-11':'high Fraser flow spring tide winter', 
+    '2020-01-24':'high Fraser flow spring tide winter',
+    '2019-01-26':'low Fraser flow neap tide winter', '2019-01-21':'low Fraser flow spring tide winter',
+    '2020-01-03':'high Fraser flow neap tide winter',
+    '2019-07-10':'low Fraser flow neap tide summer', '2019-07-16':'low Fraser flow spring tide summer', 
+    '2020-07-12':'high Fraser flow neap tide summer', '2020-07-22':'high Fraser flow spring tide summer',
+    '2019-07-25':'low Fraser flow neap tide summer', '2019-07-04':'low Fraser flow spring tide summer',
+    '2020-07-13':'high Fraser flow neap tide summer', '2020-07-21':'high Fraser flow spring tide summer',
 }
 
 locations = {
     '5b': 'Turn Point', '4a': 'Active Pass', '6a': 'English Bay', '7a': 'Juan de Fuca'
 }
+
+simulations = { 'low Fraser flow spring tide winter': 1, 'low Fraser flow neap tide winter':2,
+                'high Fraser flow spring tide winter':3, 'high Fraser flow neap tide winter':4,
+                'low Fraser flow spring tide summer': 1, 'low Fraser flow neap tide summer':2,
+                'high Fraser flow spring tide summer':3, 'high Fraser flow neap tide summer':4,}
